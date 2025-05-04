ff25431578d6ba690bb71b4e9e4d273871b6ee42
gigtarget <jatingots9@gmail.com>
2025-05-04 00:29:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
 
-# Get all sensitive data from Railway variables
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:CVyBMCpcoDAJJJfOstlYhsggkaouBuTK@yamanote.proxy.rlwy.net:32802/railway"
-)
-
-INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME", "your_instagram_username")
-INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD", "your_instagram_password")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your_openai_key")
+DATABASE_URL = os.getenv("DATABASE_URL")
+INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
+INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
