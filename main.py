5ee72adc2297acee2ed4e2f10a2dcaf32165073d
gigtarget <jatingots9@gmail.com>
2025-05-11 00:59:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def run_bot():
         send_telegram_alert(f"❌ Bot crashed: {e}")
 
 # Schedule times (UTC)
-schedule.every().day.at("04:46").do(run_bot)
+schedule.every().day.at("05:01").do(run_bot)
 schedule.every().day.at("14:00").do(run_bot)
 schedule.every().day.at("18:00").do(run_bot)
 
